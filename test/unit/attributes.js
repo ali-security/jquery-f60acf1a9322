9d7e4166b8fae63130e5fc1ff83e09bcac1ec39b
Seal <info@sealsecurity.io>
2026-09-15 09:16:51 +0000

    CVE-2016-10707

--- test/unit/attributes.js
+++ test/unit/attributes.js
@@ -477,7 +477,9 @@ test( "attr(String, Object)", function() {
 test( "attr - extending the boolean attrHandle", function() {
 	expect( 1 );
 	var called = false,
-		_handle = jQuery.expr.attrHandle.checked || $.noop;
+		origAttrHandleHadChecked = "checked" in jQuery.expr.attrHandle,
+		origAttrHandleChecked = jQuery.expr.attrHandle.checked,
+		_handle = origAttrHandleChecked || $.noop;
 	jQuery.expr.attrHandle.checked = function() {
 		called = true;
 		_handle.apply( this, arguments );
@@ -486,6 +488,12 @@ test( "attr - extending the boolean attrHandle", function() {
 	called = false;
 	jQuery( "input" ).attr( "checked" );
 	ok( called, "The boolean attrHandle does not drop custom attrHandles" );
+
+	if ( origAttrHandleHadChecked ) {
+		jQuery.expr.attrHandle.checked = origAttrHandleChecked;
+	} else {
+		delete jQuery.expr.attrHandle.checked;
+	}
 });
 
 test( "attr(String, Object) - Loaded via XML document", function() {
@@ -1475,4 +1483,30 @@ test( "Insignificant white space returned for $(option).val() (#14858)", functio
 
 	val = jQuery( "<option>  test  </option>" ).val();
 	equal( val.length, 4, "insignificant white-space returned for value" );
+});
+
+test( "non-lowercase boolean attribute getters should not crash (gh-3133)", function() {
+	expect( 6 );
+
+	var elem = jQuery( "<input checked required autofocus type='checkbox'>" );
+
+	jQuery.each({
+		checked: "Checked",
+		required: "requiRed",
+		autofocus: "AUTOFOCUS"
+	}, function( lowercased, original ) {
+		try {
+			strictEqual( elem.attr( original ), lowercased,
+				"The '" + original + "' attribute getter should return the lowercased name" );
+		} catch ( e ) {
+			ok( false, "The '" + original + "' attribute getter threw" );
+		}
+
+		try {
+			strictEqual( jQuery.find.attr( elem[ 0 ], original, false ), lowercased,
+				"jQuery.find.attr( '" + original + "' ) should return the lowercased name" );
+		} catch ( e2 ) {
+			ok( false, "jQuery.find.attr( '" + original + "' ) threw" );
+		}
+	});
 });
